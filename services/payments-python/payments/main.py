c0ef48d31366a829b0608cf1995f7d9096ec1e50
P Premnath <Premnath@bighammer.ai>
2025-09-02 02:38:15 +0530

    Replace transitions with statemachine

--- services/payments-python/payments/main.py
+++ services/payments-python/payments/main.py
@@ -22,7 +22,7 @@ import structlog
 from opentelemetry import trace
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from opentelemetry.instrumentation.grpc import GrpcInstrumentorClient
-from transitions import State, StateMachine
+from statemachine import StateMachine, State
 
 # Import generated gRPC stubs
 from .pb import payments_pb2 as payment_pb2
